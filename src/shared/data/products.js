3053d819e05b79034b75ac560d4cbcd257e0c7f7
rogeriosaraceni <rsaraceni@gmail.com>
2024-07-09 12:44:48 -0300

    update: nome das imgs de slider para cover

--- src/shared/data/products.js
+++ src/shared/data/products.js
@@ -4,7 +4,7 @@ export const allProducts = [
         title: 'PSA10',
         subtitle: 'Sensor Pressão 10 bar',
         target: 'modal-PSA10',
-        image: './assets/images/produtos/PSA10/slider-PSA10.jpg',
+        image: './assets/images/produtos/PSA10/cover.jpg',
         alt: 'PSA10',
         linkShopify: 'https://masterfuel.myshopify.com/products/PSA10'
     },
