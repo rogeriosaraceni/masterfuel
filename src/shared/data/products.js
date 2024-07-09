15d2b1d756a22fac2a07fc889e15eb88b75778bc
rogeriosaraceni <rsaraceni@gmail.com>
2024-07-09 12:42:22 -0300

    create: data/products e organizando em map + array

---
+++ src/shared/data/products.js
@@ -0,0 +1,12 @@
+// src/data/products.js
+export const allProducts = [
+    {
+        title: 'PSA10',
+        subtitle: 'Sensor Pressão 10 bar',
+        target: 'modal-PSA10',
+        image: './assets/images/produtos/PSA10/slider-PSA10.jpg',
+        alt: 'PSA10',
+        linkShopify: 'https://masterfuel.myshopify.com/products/PSA10'
+    },
+
+];
