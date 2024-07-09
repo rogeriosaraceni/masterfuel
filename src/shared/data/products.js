5c560773e8a79025b4a2d0b7e38c4beaaa246aac
rogeriosaraceni <rsaraceni@gmail.com>
2024-07-09 16:32:55 -0300

    ajustes

--- src/shared/data/products.js
+++ src/shared/data/products.js
@@ -1,12 +1,13 @@
 // src/data/products.js
 export const allProducts = [
     {
+        id: 'PSA10',
         title: 'PSA10',
         subtitle: 'Sensor Pressão 10 bar',
         target: 'modal-PSA10',
         image: './assets/images/produtos/PSA10/cover.jpg',
         alt: 'PSA10',
-        linkShopify: 'https://masterfuel.myshopify.com/products/PSA10'
+        linkShopify: 'https://masterfuel.myshopify.com/products/PSA10',
     },
 
 ];
