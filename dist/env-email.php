19be94afaaab9bcbdc9c24f49e75ebba11dccaac
rogeriosaraceni <rsaraceni@gmail.com>
2024-07-10 13:40:18 -0300

    update

--- dist/env-email.php
+++ dist/env-email.php
@@ -18,8 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["action"]) && $_POST["a
     // Cabeçalhos do e-mail
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-    $headers .= "From: " . $emailRemetente . "\r\n"; // Remetente
-    $headers .= "Return-Path: " . $email . "\r\n"; // Return-path
+    $headers .= "From: " . $email . "\r\n"; // Remetente
+    $headers .= "Return-Path: " . $emailRemetente . "\r\n"; // Return-path
 
     // Envio do e-mail
     $envio = mail($emailRemetente, $assunto, $corpo, $headers);
