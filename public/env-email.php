5bc4e6b9ff81c43da62454b852e6e31010718866
rogeriosaraceni <rsaraceni@gmail.com>
2024-07-10 13:15:01 -0300

    env email com php

---
+++ public/env-email.php
@@ -0,0 +1,35 @@
+<?php
+// Definindo variáveis do formulário
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["action"]) && $_POST["action"] == "ok") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $mensagem = nl2br($_POST["mensagem"]); // Converter quebras de linha para <br>
+
+    // Definindo variáveis de e-mail
+    $emailRemetente = "dev@masterfuel.com.br";
+    $assunto = "Formulário Site";
+
+    // Construindo o corpo do e-mail em formato HTML
+    $corpo = "<html><body>";
+    $corpo .= "<b>Nome:</b> " . $nome . "<br>";
+    $corpo .= "<b>E-mail:</b> " . $email . "<br><br>";
+    $corpo .= "<b>Mensagem:</b><br>" . $mensagem;
+
+    // Cabeçalhos do e-mail
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+    $headers .= "From: " . $emailRemetente . "\r\n"; // Remetente
+    $headers .= "Return-Path: " . $emailRemetente . "\r\n"; // Return-path
+
+    // Envio do e-mail
+    $envio = mail("masterfuel@masterfuel.com.br", $assunto, $corpo, $headers);
+
+    if ($envio) {
+        // Redirecionamento para index.html
+        header("Location: index.html");
+        exit; // Certifique-se de parar a execução do script após o redirecionamento
+    } else {
+        echo "A mensagem não pode ser enviada";
+    }
+}
+?>
